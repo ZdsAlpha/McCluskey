90e7d1a43733462b543951987b1cc78255905216
ZDS Alpha <abzsoft999@hotmail.com>
2018-04-04 03:14:59 +0500

    added terms processing

--- Program.cs
+++ Program.cs
@@ -9,6 +9,21 @@ namespace McCluskey
     {
         static void Main(string[] args)
         {
+            string raw_minterms = Input("Enter min-terms:");
+            string raw_dontcares = Input("Enter dont cares:");
+            int[] mins = Process(raw_minterms);
+            int[] donts = Process(raw_dontcares);
+
+        }
+        static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        static int[] Process(string terms)
+        {
+            if (terms == "") return new int[0];
+            return terms.Replace(" ", "").Split(",".ToCharArray()).Select(s => int.Parse(s)).ToArray();
         }
     }
 }
