d78daf89101742d3a34074bf8a214fc639087490
ZDS Alpha <abzsoft999@hotmail.com>
2018-04-04 03:03:31 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace McCluskey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
